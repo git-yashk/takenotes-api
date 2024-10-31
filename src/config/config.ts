01bdb2ec82a25dc3469d4d542ca78025b05aed6d
git-yashk <yashk.n6+github@gmail.com>
2024-10-31 23:30:26 +0530

    add function to generate access and refresh tokens

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,12 @@ const _config = {
     mongodb: {
         connection_string: process.env.MONGODB_CONNECTION_STRING,
         db_name: process.env.MONGODB_DB_NAME,
+    },
+    token: {
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+        access_token_expiry: process.env.ACCESS_TOKEN_EXPRIY,
+        refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
+        refresh_token_expiry: process.env.REFRESH_TOKEN_EXPIRY,
     }
 
 }
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "../config/config";
 
 const userSchema = new mongoose.Schema(
     {
@@ -35,6 +37,30 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password: string) {
     await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        config.token.access_token_secret as string,
+        {
+            expiresIn: config.token.access_token_expiry
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        config.token.refresh_token_secret as string,
+        {
+            expiresIn: config.token.refresh_token_expiry
+        }
+    )
 }
 
 export default mongoose.model("User", userSchema);
