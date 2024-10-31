f0ddf54ad6bc25887b0add4cf1083811cc325f26
git-yashk <yashk.n6+github@gmail.com>
2024-10-31 22:05:17 +0530

    add function to encrypt and compare password

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
     {
@@ -23,5 +24,17 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true, }
 );
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password: string) {
+    await bcrypt.compare(password, this.password);
+}
 
 export default mongoose.model("User", userSchema);
