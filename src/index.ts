aa378ad0488f96f768c0db8b2a237c66b4bf0988
git-yashk <yashk.n6+github@gmail.com>
2024-10-31 13:42:25 +0530

    express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("test");
+import app from "./app";
+import { config } from "./config/config";
+
+function startServer() {
+    const port = config.port;
+
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+    });
+}
+
+startServer();
