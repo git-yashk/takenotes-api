d8c0361216fb216599f2a28f87c0f81dfaabaffa
git-yashk <yashk.n6+github@gmail.com>
2024-11-01 16:39:21 +0530

    add create note functionality

--- src/controllers/note.controller.ts
+++ src/controllers/note.controller.ts
@@ -1,7 +1,28 @@
 import { Request, Response } from "express";
+import noteModel from "../models/note.model";
 
 async function createNote(req: Request, res: Response) {
-    res.json({ message: "createNote" });
+    const { _id, title, content, bg_color } = req.body;
+    console.log({ _id, title, content, bg_color });
+
+    if (!title && !content) {
+        res.status(400).json({ message: "Either title or content is required." });
+        return;
+    }
+
+    try {
+        const newNote = await noteModel.create({
+            title,
+            content,
+            bg_color,
+            user: _id
+        });
+        res.status(201).json(newNote);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong." });
+        return;
+    }
 }
 
 async function getNotes(req: Request, res: Response) {
